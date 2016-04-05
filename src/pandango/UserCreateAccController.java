4b08368493ffce6764af58f9f0ffea87a8576a00
Sara <sara.jacks17@yahoo.com>
2016-04-05 02:22:27 -0400

    Started creating Login Screen. Ant Build for now.

---
+++ src/pandango/UserCreateAccController.java
@@ -0,0 +1,24 @@
+package pandango;
+
+import java.net.URL;
+import java.util.ResourceBundle;
+import javafx.fxml.FXML;
+import javafx.fxml.Initializable;
+import javafx.scene.control.TextField;
+import javafx.scene.control.Tooltip;
+
+/**
+ * FXML Controller class
+ *
+ * @author Sara
+ */
+public class UserCreateAccController implements Initializable {
+    /**
+     * Initializes the controller class.
+     */
+    @Override
+    public void initialize(URL url, ResourceBundle rb) {
+        // TODO
+    }    
+    
+}
